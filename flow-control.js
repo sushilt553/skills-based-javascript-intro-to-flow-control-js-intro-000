a6e9c503a0fdf7ac4f505e92b92803538c215935
Sushil Thapa <sushilt553@gmail.com>
2019-09-26 17:40:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,10 @@
 function basicTeenager(age) {
-  if(age>13&&age<19){
+  if(age>13||age<19){
   return "You are a teenager!";
-  }
 }
 
 function teenager(age) {
-  if(age<13&&age>19){
-    return undefined;
-  }
+
 }
 
 function ageChecker(age) {
