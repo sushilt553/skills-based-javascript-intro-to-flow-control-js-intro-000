705268f8d8443474ff061c994352f0bbbf6e90f6
Sushil Thapa <sushilt553@gmail.com>
2019-09-26 17:46:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,11 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+if(age>12 && age<20){
+  return "You are a teenager!";
+}else{
+  return "You are not a teenager";
+}
 }
 
 function switchAge(age) {
